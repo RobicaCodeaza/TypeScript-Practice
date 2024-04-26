592aabf3b8c9531e0c347c6a52aa88a6a54b70bd
Stoica Robert <stoicarobert38@yahoo.com>
2024-04-26 22:09:57 +0300

    Part 1 - Finished

---
+++ Section 3/src/App.tsx
@@ -0,0 +1,12 @@
+import CourseGoal from './Components/CourseGoal';
+
+export default function App() {
+  return (
+    <main>
+      <CourseGoal title='Learn React + Ts'>
+        Learn it from the ground up
+      </CourseGoal>
+      {/* <CourseGoal></CourseGoal> */}
+    </main>
+  );
+}
